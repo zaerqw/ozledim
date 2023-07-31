01516389833b8c10c140c98406971e0e8aa9d67f
zozobee <96136237+zaerqw@users.noreply.github.com>
2023-07-31 15:54:38 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x537e6a020447599a18bade075bf25bc24702f677";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
